5da75b16bcce11b5104c8ffc4e5fe81ec9ac9434
Satoshi Scott Iwako <iwakoscott@gmail.com>
2018-03-15 11:48:58 -0700

    add josefins font to index.css, import into App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import MissionControl from './MissionControl';
+import '../index.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1 className="display-1">Hello, world</h1>
+        <MissionControl />
       </div>
     );
   }
