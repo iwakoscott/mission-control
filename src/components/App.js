8ce9dfa656c8d9f965cd671f21293038bca2ab93
Satoshi Scott Iwako <iwakoscott@gmail.com>
2018-03-15 10:22:14 -0700

    add bootstrap, import into App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello, world!
+        <h1 className="display-1">Hello, world</h1>
       </div>
     );
   }
