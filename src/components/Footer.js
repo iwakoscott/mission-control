0414d03210e86d2e28da95d68be479e8fb2c11b8
Satoshi Scott Iwako <iwakoscott@gmail.com>
2018-03-19 14:04:03 -0700

    add twitter tag to footer and add styling to match twitter blue

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,6 +7,12 @@ const Footer = props =>
         <p className="small spaced-out" style={{color: '#2f3640'}}>Made by Satoshi</p>
       </div>
     </div>
+    <div className="row">
+      <div className="col col-sm-6 offset-sm-3 text-center">
+        <i className="fa fa-twitter mr-2" style={{"color": "#00aced", "verticalAlign": "middle"}}></i>
+        <a className="small at-tag spaced-out-lower" href="https://twitter.com/theisomorphic">&#64;{"theisomorphic"}</a>
+      </div>
+    </div>
   </div>
 
 export default Footer;
