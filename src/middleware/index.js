57a7b486cbb155df4c9d617a6272c5d269a8ce10
Satoshi Scott Iwako <iwakoscott@gmail.com>
2018-03-22 16:57:13 -0700

    remove redux tool from middleware

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,5 +1,5 @@
-import { applyMiddleware, compose } from 'redux';
+import { applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-export default composeEnhancers(applyMiddleware(thunk));
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+export default applyMiddleware(thunk);
